db2a4423b181b290b4c93f07942085c679cb49a0
Stanislav <s.pluzhnick@gmail.com>
2020-12-09 16:34:17 +0200

    created local storage service

---
+++ SimpleChat.Client/wwwroot/jsinterop.js
@@ -0,0 +1,11 @@
+﻿function set(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function get(key) {
+    localStorage.getItem(key);
+}
+
+function remove(key) {
+    localStorage.removeItem(key);
+}
