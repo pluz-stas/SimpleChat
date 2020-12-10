5c96bad23647739be29f694e7282b4ae9b756445
Stanislav <s.pluzhnick@gmail.com>
2020-12-10 14:22:49 +0200

    created user state managment

--- SimpleChat.Client/Infrastructure/ILocalStorageService.cs
+++ SimpleChat.Client/Infrastructure/ILocalStorageService.cs
@@ -5,9 +5,13 @@ namespace SimpleChat.Client.Infrastructure
     public interface ILocalStorageService
     {
         Task SetAsync<T>(string key, T item) where T : class;
+
         Task SetStringAsync(string key, string value);
+
         Task<T> GetAsync<T>(string key) where T : class;
+
         Task<string> GetStringAsync(string key);
+
         Task RemoveAsync(string key);
     }
 }
--- SimpleChat.Client/Infrastructure/LocalStorageService.cs
+++ SimpleChat.Client/Infrastructure/LocalStorageService.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.JSInterop;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.JSInterop;
 
 namespace SimpleChat.Client.Infrastructure
 {
@@ -16,27 +13,6 @@ namespace SimpleChat.Client.Infrastructure
             _jsRuntime = jsRuntime;
         }
 
-        public async Task<T> GetAsync<T>(string key) where T : class
-        {
-            var data = await _jsRuntime.InvokeAsync<string>("get", key);
-            if(string.IsNullOrEmpty(data))
-            {
-                return null;
-            }
-            return JsonSerializer.Deserialize<T>(data)!;
-        } 
-
-        public async Task<string> GetStringAsync(string key)
-        {
-            return await _jsRuntime.InvokeAsync<string>("get", key);
-        }
-
-        public Task RemoveAsync(string key)
-        {
-            _jsRuntime.InvokeAsync<string>("remove", key);
-            return Task.CompletedTask;
-        }
-
         public async Task SetAsync<T>(string key, T item) where T : class
         {
             var data = JsonSerializer.Serialize(item);
@@ -46,6 +22,27 @@ namespace SimpleChat.Client.Infrastructure
         public Task SetStringAsync(string key, string value)
         {
             _jsRuntime.InvokeAsync<string>("set", key, value);
+            return Task.CompletedTask;
+        }
+
+        public async Task<T> GetAsync<T>(string key) where T : class
+        {
+            var data = await _jsRuntime.InvokeAsync<string>("get", key);
+            if (string.IsNullOrEmpty(data))
+            {
+                return null!;
+            }
+            return JsonSerializer.Deserialize<T>(data)!;
+        }
+
+        public async Task<string> GetStringAsync(string key)
+        {
+            return await _jsRuntime.InvokeAsync<string>("get", key);
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            _jsRuntime.InvokeAsync<string>("remove", key);
             return Task.CompletedTask;
         }
     }
