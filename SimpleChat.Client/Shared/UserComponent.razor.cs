270e149163fe6b5ba0a18c14ba47036bbad77eee
Stanislav <s.pluzhnick@gmail.com>
2020-12-21 19:37:02 +0200

    Updated user component

--- SimpleChat.Client/Shared/UserComponent.razor.cs
+++ SimpleChat.Client/Shared/UserComponent.razor.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System;
+using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using SimpleChat.Client.Infrastructure;
 using SimpleChat.Client.Models;
@@ -11,16 +13,67 @@ namespace SimpleChat.Client.Shared
         [Parameter(CaptureUnmatchedValues = true)]
         public Dictionary<string, object> InputAttributes { get; set; }
         [Inject] private ILocalStorageService LocalStorageService { get; set; }
-        private const string UserStateKeyName = "UserState";
-        public UserComponent()
+        
+        private const string UserNameKeyName = "UserName";
+        private const string UserImgKeyName = "UserImgUrl";
+        
+        private bool isUserNameInputOpen;
+        private bool isUserImgInputOpen;
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Too Long")]
+        private string UserName { get; set; }
+        private string UserImg { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
-            UserData = new UserModel();
+            string localStorageName = await LocalStorageService.GetStringAsync(UserNameKeyName);
+            if (localStorageName == null)
+            {
+                UserName = "anon";
+            }
+            else
+            {
+                UserName = localStorageName;
+            }
+            UserImg = await LocalStorageService.GetStringAsync(UserImgKeyName);
+            await base.OnInitializedAsync();
         }
         
-        private UserModel UserData { get; set; }
-        private async Task SetAsync()
+        private async Task SetNameAsync()
         {
-            await LocalStorageService.SetAsync(UserStateKeyName, UserData);
+            if (UserName != null)
+            {
+                await LocalStorageService.SetStringAsync(UserNameKeyName, UserName);
+            }
+        }       
+        
+        private async Task SetImgAsync()
+        {
+            if (UserImg != null)
+            {
+                await LocalStorageService.SetStringAsync(UserImgKeyName, UserName);
+            }
+        }
+        
+        private async Task ToggleEditUserNameInputAsync()
+        {
+            if (!isUserImgInputOpen)
+            {
+                await SetNameAsync();
+            }
+            isUserNameInputOpen = !isUserNameInputOpen;
+            isUserImgInputOpen = !isUserImgInputOpen && isUserImgInputOpen;
+        }
+
+        private async Task ToggleEditUserImgInputAsync()
+        {
+            if (!isUserImgInputOpen)
+            {
+                await SetImgAsync();
+            }
+            isUserImgInputOpen = !isUserImgInputOpen;
+            isUserNameInputOpen = isUserNameInputOpen && !isUserNameInputOpen;
         }
     }
 }
